b61c5b7bd1879dd0066fe1e40eb4d7723ccb7a79
ptagell <ptagell@gmail.com>
2017-01-07 18:08:36 +1100

    clean up filenames and remove unused code post testing

--- water.py
+++ water.py
@@ -6,19 +6,16 @@ import grovepi
 # Connect the Grove Relay to digital port D5. LED to D8
 # SIG,NC,VCC,GND
 relay = 5
-led = 8
 
 grovepi.pinMode(relay,"OUTPUT")
 
 # switch on for 5 seconds
 print ("Turning on the water")
 grovepi.digitalWrite(relay,1)
-grovepi.digitalWrite(led,1)
 print ("Water is on")
-time.sleep(100) # This is the duration of watering in minutes.
+time.sleep(40) # This is the duration of watering in minutes.
 
 # switch off for 5 seconds
 print ("Ending...turning off water")
 grovepi.digitalWrite(relay,0)
-grovepi.digitalWrite(led,0)
 print ("Water is off")
