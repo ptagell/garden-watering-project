af1539c87035e1c3e0fbf9a0f048565110e46d31
Paul Tagell <ptagell@gmail.com>
2017-01-16 07:48:53 +1100

    uncomment code after testing

--- water.py
+++ water.py
@@ -16,13 +16,13 @@ print(sys.argv[1:][0])
 # Convert the watering duration value to a float as expected per sleep function.
 duration_of_watering_as_float = float(duration_of_watering_in_seconds)
 
-# grovepi.pinMode(relay,"OUTPUT")
+grovepi.pinMode(relay,"OUTPUT")
 
 print duration_of_watering_as_float
 
 print ("Attempting to turn on the water")
 # Turn the relay to the on position
-    # grovepi.digitalWrite(relay,1)
+grovepi.digitalWrite(relay,1)
 # Confirm the water is on. Note: Not sure how to verify this without some sort of flow sensor.
 print ("Water is on")
 # This is the duration of watering in seconds.
@@ -31,5 +31,5 @@ time.sleep(duration_of_watering_as_float)
 
 print ("Ending...trying to turning off water")
 # Turn the relay off.
-    # grovepi.digitalWrite(relay,0)
+grovepi.digitalWrite(relay,0)
 print ("Water is off")
