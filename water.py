d41f1fef9332da15a23519c371674f98c0ebb057
Paul Tagell <ptagell@gmail.com>
2017-01-15 17:28:00 +1100

    make duration of watering a float to see if it works

--- water.py
+++ water.py
@@ -8,15 +8,19 @@ import grovepi
 # Connect the Grove Relay to digital port D5. LED to D8
 # SIG,NC,VCC,GND
 relay = 5
-duration_of_watering = 120
+# Specify the duration of watering in seconds
+duration_of_watering_in_seconds = 120
+duration_of_watering_as_float = float(duration_of_watering_in_seconds)
 
 grovepi.pinMode(relay,"OUTPUT")
 
-# switch on for 5 seconds
-print ("Turning on the water")
+print ("Attempting to turn on the water")
+# Turn the relay to the on position
 grovepi.digitalWrite(relay,1)
+# Confirm the water is on. Note: Not sure how to verify this without some sort of flow sensor.
 print ("Water is on")
-time.sleep(120) # This is the duration of watering in seconds.
+# This is the duration of watering in seconds.
+time.sleep(duration_of_watering_as_float)
 
 # switch off for 5 seconds
 print ("Ending...turning off water")
