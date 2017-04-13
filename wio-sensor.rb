231a6b245a51161363cdbe3530532d17358407fe
Paul Tagell <ptagell@gmail.com>
2017-04-13 14:11:54 +1000

    fix variable

--- wio-sensor.rb
+++ wio-sensor.rb
@@ -1,7 +1,7 @@
 require 'dotenv/load'
 
 current_hour = Time.now.hour
-unless current_time == 5
+unless current_hour == 5
   system("curl -k -X POST https://us.wio.seeed.io/v1/node/pm/sleep/200?access_token=#{ENV['WIO_TOKEN']}")
 else
   # Do nothing. Watering script will control for this hour.
