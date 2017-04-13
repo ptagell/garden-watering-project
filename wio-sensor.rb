1a34d57410257a93890cb303ec80c02bef288921
Paul Tagell <ptagell@gmail.com>
2017-04-13 15:03:24 +1000

    update script to output status

--- wio-sensor.rb
+++ wio-sensor.rb
@@ -1,8 +1,10 @@
 require 'dotenv/load'
 
 current_hour = Time.now.hour
+puts Time.now.localtime.to_s+" Checking time and putting to sleep if required"
 unless current_hour == 5
   system("curl -k -X POST https://us.wio.seeed.io/v1/node/pm/sleep/3590?access_token=#{ENV['WIO_TOKEN']}")
+  puts Time.now.localtime.to_s+" Put to sleep for this hour"
 else
-  # Do nothing. Watering script will control for this hour.
+  puts Time.now.localtime.to_s+" Rise and shine. Ready to water."
 end
