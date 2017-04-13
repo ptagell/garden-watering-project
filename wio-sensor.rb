90293c2cc7a066418d1d2ebb7e60afaf3d0b2fde
Paul Tagell <ptagell@gmail.com>
2017-04-13 16:08:39 +1000

    comment

--- wio-sensor.rb
+++ wio-sensor.rb
@@ -3,6 +3,7 @@ require 'dotenv/load'
 current_hour = Time.now.hour
 puts Time.now.localtime.to_s+" Checking time and putting to sleep if required"
 unless current_hour == 5
+  # Put sensor to sleep until 20 seconds before the hour (to allow time for it to connect to wifi)
   remainder_of_hour = ((59-Time.now.min)*60)+40
   system("curl -k -X POST https://us.wio.seeed.io/v1/node/pm/sleep/#{remainder_of_hour}?access_token=#{ENV['WIO_TOKEN']}")
   puts Time.now.localtime.to_s+" Have put system to sleep for the "+remainder_of_hour.to_s+"seconds remaining in the hour"
