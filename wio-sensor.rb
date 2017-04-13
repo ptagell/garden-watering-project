bd8e8e36cdf5d7ae71fcc316a91a3e45f0cd7336
Paul Tagell <ptagell@gmail.com>
2017-04-13 16:07:43 +1000

    fix timing script to allow it to be run whenever

--- wio-sensor.rb
+++ wio-sensor.rb
@@ -3,8 +3,9 @@ require 'dotenv/load'
 current_hour = Time.now.hour
 puts Time.now.localtime.to_s+" Checking time and putting to sleep if required"
 unless current_hour == 5
-  system("curl -k -X POST https://us.wio.seeed.io/v1/node/pm/sleep/3590?access_token=#{ENV['WIO_TOKEN']}")
-  puts Time.now.localtime.to_s+" Put to sleep for this hour"
+  remainder_of_hour = ((59-Time.now.min)*60)+40
+  system("curl -k -X POST https://us.wio.seeed.io/v1/node/pm/sleep/#{remainder_of_hour}?access_token=#{ENV['WIO_TOKEN']}")
+  puts Time.now.localtime.to_s+" Have put system to sleep for the "+remainder_of_hour.to_s+"seconds remaining in the hour"
 else
   puts Time.now.localtime.to_s+" Rise and shine. Ready to water."
 end
