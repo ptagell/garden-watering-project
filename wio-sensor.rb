d440119255c1627864f763db046c681d45afa816
Paul Tagell <ptagell@gmail.com>
2017-04-21 07:27:08 +1000

    update sensor code to put for sleep for longer to try and conserve battery life

--- wio-sensor.rb
+++ wio-sensor.rb
@@ -1,12 +1,26 @@
 require 'dotenv/load'
+require 'DateTime'
+now = DateTime.now
+puts DateTime.now.localtime.to_s+" Checking time and putting to sleep if required"
+target_time = DateTime.new(now.year, now.month, now.day, 4, 59, 50, now.zone)
+seconds_till_target_time = ((target_time-now)*24*60*60).to_i
 
-current_hour = Time.now.hour
-puts Time.now.localtime.to_s+" Checking time and putting to sleep if required"
-unless current_hour == 5
-  # Put sensor to sleep until 20 seconds before the hour (to allow time for it to connect to wifi)
-  remainder_of_hour = ((59-Time.now.min)*60)+40
-  system("curl -k -X POST https://us.wio.seeed.io/v1/node/pm/sleep/#{remainder_of_hour}?access_token=#{ENV['WIO_TOKEN']}")
-  puts Time.now.localtime.to_s+" Have put system to sleep for the "+remainder_of_hour.to_s+"seconds remaining in the hour"
+# Sleep time is just under 5 hours. Max sleep time for Wio node is 5 hours.
+max_sleep_duration_seconds = 17000
+
+unless now.hour == target_time.hour+1
+  if seconds_till_target_time > max_sleep_duration_seconds
+    sleep_duration = max_sleep_duration_seconds
+  else
+    sleep_duration = seconds_till_target_time
+  end
+  # system("curl -k -X POST https://us.wio.seeed.io/v1/node/pm/sleep/#{sleep_duration}?access_token=#{ENV['WIO_TOKEN']}")
+  puts Time.now.localtime.to_s+" Have put system to sleep for "+sleep_duration.to_s+" seconds."
 else
   puts Time.now.localtime.to_s+" Rise and shine. Ready to water."
 end
+
+
+
+- [ ] Solve issue with target time being set to earlier in the same day
+- [ ] Solve issue with 5am wake up (and potential sleeping till next day)
